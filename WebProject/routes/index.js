9b1f1896d4a741568489d3aa547845b769ffb508
kapiraj56 <160338459+kapiraj56@users.noreply.github.com>
2024-11-05 15:21:08 -0500

    setting up index

--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home ' });
+});
+
+/* GET About me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About me ' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects ' });
+});
+
+/* GET Contact us page. */
+router.get('/contactus', function(req, res, next) {
+  res.render('index', { title: 'Contact us ' });
 });
 
 module.exports = router;
