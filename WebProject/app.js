02a6f474267a9d1c7fc9df265cdd11761aea9074
kapiraj56 <160338459+kapiraj56@users.noreply.github.com>
2024-11-05 18:47:38 -0500

    cleaning up the index.ejs file

--- WebProject/app.js
+++ WebProject/app.js
@@ -10,8 +10,6 @@ let  usersRouter = require('./routes/users');
 let  app = express();
 
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 router.get('/',function(req,res,next) {
   res.render('index',{
@@ -7,28 +7,32 @@ router.get('/',function(req,res,next) {
   });
 });
 
+module.exports = router;
+
 router.get('/home',function(req,res,next) {
   res.render('index',{
     title: 'Home'
   });
 });
+module.exports = router;
 
 router.get('/aboutme',function(req,res,next) {
   res.render('index',{
     title: 'Aboutme'
   });
 });
+module.exports = router;
 
 router.get('/projects',function(req,res,next) {
   res.render('index',{
     title: 'Projects'
   });
 });
+module.exports = router;
 
 router.get('/contactus',function(req,res,next) {
   res.render('index',{
     title: 'Contact us'
   });
 });
-
 module.exports = router;
